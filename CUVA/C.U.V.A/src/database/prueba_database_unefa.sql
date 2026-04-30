823229a0bd27378d593af8284633b38fd5ce3361
Jose M <josembetancourtc@gmail.com>
2026-04-30 13:00:09 -0400

    prueba_database_unefa.sql

--- CUVA/C.U.V.A/src/database/prueba_database_unefa.sql
+++ CUVA/C.U.V.A/src/database/prueba_database_unefa.sql
@@ -1,31 +1,41 @@
-
 create database database_unefa;
 
 use database_unefa;
 create table User
 (
-idUser int primary key not null,
+IDUser int primary key not null,
 Name varchar(50) not null,
 LastName varchar(50),
 UserName varchar(50),
-Password varchar(50),
 Email varchar(50),
+Password varchar(50),
 Post varchar(50)
 );
 
-create table Materia
+create table Subject
 (
-IDMateria int primary key not null,
-Nombre varchar(50),
-Codigo_Materia varchar(50),
+IDSubject int primary key not null,
+Name varchar(50),
 Unit_credit varchar(50),
-semestre varchar(50)
+Semester varchar(50)
+);
+
+create table Student
+(
+IDStudent int primary key not null,
+Name varchar(50),
+Lastname varchar(50),
+Career varchar(50),
+Tuition varchar(50)
 );
 
 use database_unefa;
-insert into user(Lastname,Name)
-values("b","j");
+insert into user(IDUser,Name,Lastname,Username,Email,Password,Post)
+values("32066670","Jose Miguel","Betancourt Castro","Jose M","Gmail.con","1234","Hotmail");
 
-insert into Materia(Nombre,Codigo_Materia)
-values("a","b")
+insert into Subject(IDSubject,Name,Unit_credit,Semester)
+values("2613 N","Lenguaje III","9999999","6to");
+
+insert into Student(IDStudent,Name,Lastname,Career,Tuition)
+values("12345678","Marco","Perez","politica","N1");
 
