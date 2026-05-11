e49949d4e407ed1c17225800d407c7afc0567032
CUVA-dev <c.u.v.a202600@gmail.com>
2026-05-11 11:48:23 -0400

    templates: login and register

---
+++ CUVA/C.U.V.A/src/controller/RegisterController.java
@@ -0,0 +1,30 @@
+
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent; 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+
+public class RegisterController implements Initializable {
+    
+    private Button btnRegister;
+    
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void accionRegister(ActionEvent event) {
+        System.out.println("---------------------------------");
+        System.out.println("¡El botón funciona correctamente!");
+        System.out.println("---------------------------------");
+    }
+}
