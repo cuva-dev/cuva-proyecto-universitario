1c2bed085d323609a89c8fa18f04bfdb28b03eb8
CUVA-dev <c.u.v.a202600@gmail.com>
2026-06-04 14:20:32 -0400

    adelanto de login

--- CuvaProject/src/main/java/com/mycompany/cuvaproject/controller/LoginController.java
+++ CuvaProject/src/main/java/com/mycompany/cuvaproject/controller/LoginController.java
@@ -12,6 +12,7 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.TextField;
 
 public class LoginController implements Initializable {
     
@@ -22,6 +23,29 @@ public class LoginController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // Se ejecuta al cargar la vista
     }    
+    
+    
+    @FXML
+    private TextField id;
+    
+    @FXML
+    private TextField password;
+    
+    
+    private void hundleUser(){
+    
+        String idValue = id.getText();
+        String passwordValue = password.getText();
+    
+    
+        if (idValue.isEmpty() || passwordValue.isEmpty()) {
+            throw new IllegalArgumentException("los campos no pueden estar vacios");
+        }
+    
+        
+        
+        
+    }
     
     @FXML
     private void accionBoton(ActionEvent event) {
