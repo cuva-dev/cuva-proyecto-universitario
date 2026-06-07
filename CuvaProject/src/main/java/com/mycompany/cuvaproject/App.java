19ac368e27eed25699e6f252fa758f2f9db1f6d9
CUVA-dev <piterson2805@gmail.com>
2026-06-07 19:35:43 -0400

    acomodo de ventanas inicio

--- CuvaProject/src/main/java/com/mycompany/cuvaproject/App.java
+++ CuvaProject/src/main/java/com/mycompany/cuvaproject/App.java
@@ -8,17 +8,15 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-/**
- * JavaFX App codigo de prueba para ver si funciona
- */
 public class App extends Application {
 
     private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("main"), 640, 480);
+        scene = new Scene(loadFXML("main"), 1024,680);
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
