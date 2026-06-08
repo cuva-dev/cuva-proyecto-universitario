6041bf6b46cba40b4ef2e4df89079a452887e99c
Israel Suarez <jesusisraelsuarez2001@gmail.com>
2026-06-07 22:02:47 -0400

    PRUEBA

--- CuvaProject/src/main/java/com/mycompany/cuvaproject/controller/BitacoraController.java
+++ CuvaProject/src/main/java/com/mycompany/cuvaproject/controller/BitacoraController.java
@@ -1,4 +1,4 @@
-/*
+/*  PUTO EL QUE LEE
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
