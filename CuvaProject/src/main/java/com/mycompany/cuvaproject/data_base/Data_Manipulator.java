3f86dfdaf416d3e555b4406a74fcb0837f2ceeda
Jose M <josembetancourtc@gmail.com>
2026-06-10 16:55:15 -0400

    Modify user insert SQL and clean up methods
    Updated SQL insert statement to use 'IDPost' instead of 'Post' and removed unused extraction methods.

--- CuvaProject/src/main/java/com/mycompany/cuvaproject/data_base/Data_Manipulator.java
+++ CuvaProject/src/main/java/com/mycompany/cuvaproject/data_base/Data_Manipulator.java
@@ -17,7 +17,7 @@ public class Data_Manipulator {
         public void InsertTableUser(ConnectionMySQL CMySQL,User user){
 
         // Consulta SQL en este caso Insertar
-        String sql = "INSERT INTO user (Name,LastName,ID,Email,Password,Post) VALUES ('"+user.getName()+"','"+user.getLastName()+"','"+user.getID()+"','"+user.getEmail()+"','"+user.getPassword()+"','"+user.getPost()+"')";
+        String sql = "INSERT INTO user (Name,LastName,ID,Email,Password,IDPost) VALUES ('"+user.getName()+"','"+user.getLastName()+"','"+user.getID()+"','"+user.getEmail()+"','"+user.getPassword()+"','"+user.getPost()+"')";
 
         //conecta a la base de datos
         try (Connection conn = CMySQL.conectarMySQL()) {
@@ -249,102 +249,5 @@ public class Data_Manipulator {
             System.err.println("Error al modificar los datos: " + e.getMessage());
         }
     }
-    
-    
-    //metodo para extraer datos de las tablas
-
-        public void ExtractTableUser(ConnectionMySQL CMySQL,User user){
-
-        String sql = "SELECT * FROM User WHERE ID = '"+user.getID()+"'";
-        try (Connection conn = CMySQL.conectarMySQL();
-             PreparedStatement stmt = conn.prepareStatement(sql);
-             ResultSet rs = stmt.executeQuery()) {
-
-            // Iterar sobre el ResultSet para extraer los datos
-            while (rs.next()) {
-                // Extrae datos por el nombre de la columna
-                String ID = rs.getString("ID");
-                String name = rs.getString("Name");
-                String lastname = rs.getString("Lastname");
-                String email = rs.getString("email"); 
-
-                System.out.println(ID + name);
-        }
-
-        } catch (SQLException e) {
-            System.err.println("Error al consultar los datos: " + e.getMessage());
-        }
-
-    }
-    
-    public void ExtractTableSubject(ConnectionMySQL CMySQL,Subject sub){
-
-    String sql = "SELECT * FROM Subject WHERE Code = '"+sub.getCode()+"'";  
-
-    try(Connection conn = CMySQL.conectarMySQL()){
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-             ResultSet rs = pstmt.executeQuery();
-            while(rs.next()){
-                String code = rs.getString("Code");
-                String name = rs.getString("Name");
-                int semester = rs.getInt("Semester");
-                int unit_credit = rs.getInt("Unit_Credit");
-            }
-            }catch(SQLException e){
-    System.err.println("Error al consultar los datos: " + e.getMessage());
-   }
-}
-
-    public void ExtractTableStudent(ConnectionMySQL CMySQL,Student stu){
-
-    String sql = "SELECT * FROM Student WHERE ID= '"+stu.getID()+"'";
-
-    try (Connection conn = CMySQL.conectarMySQL()){
-        PreparedStatement pstmt = conn.prepareStatement(sql);
-        ResultSet rs = pstmt.executeQuery();
-
-        while(rs.next()){
-            
-            String name = rs.getString("name");
-            String lastname = rs.getString("lastname");
-            String career = rs.getString("career");
-            String tuition = rs.getString("tuition");
-        }  
-    } catch (SQLException e) {
-        e.printStackTrace();
-        
-    }
-
-
-
-    }
-    
-    public void ExtractTableReprobated(ConnectionMySQL CMySQL,Reprobated rep){
-        String sql = "SELECT stu.ID, stu.Name, sub.Code, sub.name FROM reprobated rep "
-        +" INNER JOIN Student stu ON rep.IDStudent = stu.ID "
-        +" INNER JOIN Subject sub ON rep.CodeSubject = sub.Code "
-        +" WHERE rep.IDStudent = '32066670';";
-
-        try (Connection conn = CMySQL.conectarMySQL();
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-            ResultSet rs = pstmt.executeQuery()){
-        
-        while(rs.next()){
-        int ID = rs.getInt("ID");
-        int Code = rs.getInt("Code");
-        String nameSubject = rs.getString("Name");
-        String nameStudent = rs.getString("Name");
-
-        System.out.println(ID + Code + nameStudent + nameSubject +"1");
-        }    
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-
-        
-    }
-
-
-
 }
     
