8172bf893eda8a680287ca6acfb18c428c0f7695
Jose M <josembetancourtc@gmail.com>
2026-06-17 15:20:18 -0400

    Refactor Data_Manipulator with CRUD methods and logging

--- CuvaProject/src/main/java/com/mycompany/cuvaproject/data_base/Data_Manipulator.java
+++ CuvaProject/src/main/java/com/mycompany/cuvaproject/data_base/Data_Manipulator.java
@@ -4,7 +4,6 @@ import java.sql.PreparedStatement;// permite ejecutar consultas SQL con parámet
 import java.sql.ResultSet; // maneja los resultados de las consultas SQL
 import java.sql.SQLException; // maneja los errores relacionados con SQL
 
-// estos import se pueden borrar despues en el proyecto final
 import com.mycompany.cuvaproject.models.User;
 import com.mycompany.cuvaproject.models.Student;
 import com.mycompany.cuvaproject.models.Subject;
@@ -12,9 +11,62 @@ import com.mycompany.cuvaproject.models.Reprobated;
 
 public class Data_Manipulator {
 
-    // Métodos para insertar datos en la tablas
     
-        public void InsertTableUser(ConnectionMySQL CMySQL,User user){
+    public static ArrayList<Timestamp> Atime = new ArrayList<>();
+    public static ArrayList<String> Aaction = new ArrayList<>();
+
+    
+    // Métodos de la tabla bitacora
+    
+    public void InsertTableBitacora(ConnectionMySQL CMySQL,String idValue,String action){
+
+        String sql = "Insert into bitacora (time,IDUser,action) values (current_timestamp,'"+idValue+"','"+action+"')";
+
+        try (Connection conn = CMySQL.conectarMySQL()) {
+
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void ExtractTableBitacora(ConnectionMySQL CMySQL,String IDvalue){
+
+        String sql = "select * from Bitacora where IDUser = '"+IDvalue+"'";
+        try (Connection conn = CMySQL.conectarMySQL()){
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+    
+            while (rs.next()) {
+    
+                Timestamp login = rs.getTimestamp("time");
+                String action = rs.getString("action");
+                Atime.add(login);
+                Aaction.add(action);
+            };
+    
+            System.out.println("se guardo en la bitacora");
+              for (Timestamp time : Atime) {
+                System.out.println(time);
+              }
+              for (String action : Aaction) {
+                System.out.println(action);
+              }
+    
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+    
+        }
+    
+    }
+
+    // Metodos de la tabla user
+
+    public void InsertTableUser(ConnectionMySQL CMySQL,User user){
 
         // Consulta SQL en este caso Insertar
         String sql = "INSERT INTO user (Name,LastName,ID,Email,Password,IDPost) VALUES ('"+user.getName()+"','"+user.getLastName()+"','"+user.getID()+"','"+user.getEmail()+"','"+user.getPassword()+"','"+user.getPost()+"')";
@@ -26,15 +78,57 @@ public class Data_Manipulator {
             PreparedStatement pstmt = conn.prepareStatement(sql);
             
             // el "pstmt" ejecutar la inserción
-            pstmt.executeUpdate();
-                
+              int filasAfectadas = pstmt.executeUpdate();
+                if (filasAfectadas > 0) {
+                    InsertTableBitacora(CMySQL,user.getID(),"Insertó un nuevo usuario");
+                }
         } catch (SQLException e) {
             System.out.println(e);
         }
+
     }
 
-    public void InsertTableSubject(ConnectionMySQL CMySQL,Subject sub){
+    public void DeleteInTableUser(ConnectionMySQL CMySQL,User user,String idValue){
+        // Consulta SQL en este caso Borrar
+        String sql = "DELETE FROM user WHERE ID = '"+user.getID()+"'";
+        // conecta a la base de datos
+        try (Connection conn = CMySQL.conectarMySQL()) {
+            
+            PreparedStatement pstmt = conn.prepareStatement(sql);
 
+            int filasAfectadas = pstmt.executeUpdate();
+            
+            if (filasAfectadas > 0) {
+             InsertTableBitacora(CMySQL,idValue,"Eliminó un usuario");
+            }
+        } catch (SQLException e) {
+            System.err.println("Error al eliminar datos: " + e.getMessage());
+        }
+    }
+
+    public void modifyTableUser(ConnectionMySQL CMySQL,User user,String idValue){
+
+        String sql = "UPDATE User SET Name = '"+user.getName()+"',LastName = '"+user.getLastName()+"',Email = '"+user.getEmail()+"', Password = '"+user.getPassword()+"', Post = '"+user.getPost()+"' WHERE ID = '"+user.getID()+"'";
+
+        try (Connection conn = CMySQL.conectarMySQL()){
+
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            int filasActualizadas = pstmt.executeUpdate();
+
+            if (filasActualizadas > 0) {
+                InsertTableBitacora(CMySQL,idValue,"Modificó un usuario");
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error al modificar los datos: " + e.getMessage());
+        }
+    }
+    
+    // Metodos de la tabla Subject
+
+    public void InsertTableSubject(ConnectionMySQL CMySQL,Subject sub){
+        // este metodo no guarda en la bitacora ya que es automatico con el scanner del pensum ()
         String sql = "INSERT INTO Subject (Code,Semester,Unit_Credit,Name) VALUES ('"+sub.getCode()+"','"+sub.getSemester()+"','"+sub.getUnit_credit()+"','"+sub.getName()+"')";
         
         try (Connection conn = CMySQL.conectarMySQL()) {
@@ -49,62 +143,7 @@ public class Data_Manipulator {
 
     }
 
-    public void InsertTableStudent(ConnectionMySQL CMySQL,Student stu){
-
-        String sql = "INSERT INTO Student (ID,Name,LastName,Career,Tuition) VALUES ('"+stu.getID()+"','"+stu.getName()+"','"+stu.getLastName()+"','"+stu.getCareer()+"','"+stu.getTuition()+"')";
-
-        try (Connection conn = CMySQL.conectarMySQL()){
-
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-
-            pstmt.executeUpdate();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void InsertTableReprobated(ConnectionMySQL CMySQL,Reprobated rep){
-
-        String sql = "INSERT INTO Reprobated (ID,CodeSubject) VALUES ('"+rep.getIDStudent()+"','"+rep.getCodeSubject()+"')";
-
-        try (Connection conn = CMySQL.conectarMySQL()) {
-
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-            
-            pstmt.executeUpdate();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Métodos para eliminar datos en la tablas
-
-    public void DeleteInTableUser(ConnectionMySQL CMySQL,User user){
-
-
-        // Consulta SQL en este caso Borrar
-        String sql = "DELETE FROM user WHERE ID = '"+user.getID()+"'";
-        // conecta a la base de datos
-        try (Connection conn = CMySQL.conectarMySQL()) {
-            
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-
-            int filasAfectadas = pstmt.executeUpdate();
-            
-            if (filasAfectadas > 0) {
-                System.out.println("Registro eliminado exitosamente.");
-            } else {
-                System.out.println("No se encontró ningún registro con ese ID.");
-            }
-
-        } catch (SQLException e) {
-            System.err.println("Error al eliminar datos: " + e.getMessage());
-        }
-    }
-
-    public void DeleteInTableSubject(ConnectionMySQL CMySQL,Subject sub){
+    public void DeleteInTableSubject(ConnectionMySQL CMySQL,Subject sub,String idValue){
 
         String sql = "DELETE FROM Subject WHERE code = '"+sub.getCode()+"'";
 
@@ -115,9 +154,7 @@ public class Data_Manipulator {
             int filasAfectadas = pstmt.executeUpdate();
             
             if (filasAfectadas > 0) {
-                System.out.println("Registro eliminado exitosamente.");
-            } else {
-                System.out.println("No se encontró ningún registro con ese ID.");
+                InsertTableBitacora(CMySQL,idValue,"Eliminó una materia");
             }
 
         } catch (SQLException e) {
@@ -125,7 +162,45 @@ public class Data_Manipulator {
         }
     }
 
-    public void DeleteInTableStudent(ConnectionMySQL CMySQL,Student stu){
+    public void modifyTableSubject(ConnectionMySQL CMySQL,Subject sub,String idValue){
+
+        String sql = "UPDATE Subject SET Code='"+sub.getCode()+"',Semester='"+sub.getSemester()+"',Unit_Credit='"+sub.getUnit_credit()+"',Name='"+sub.getName()+"'";
+        try (Connection conn = CMySQL.conectarMySQL()){
+
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            int filasActualizadas = pstmt.executeUpdate();
+
+            if (filasActualizadas > 0) {
+                InsertTableBitacora(CMySQL,idValue,"Modificó una materia");
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error al modificar los datos: " + e.getMessage());
+        }
+    }
+
+    // Metodos de la tabla Student
+
+    public void InsertTableStudent(ConnectionMySQL CMySQL,Student stu,String idValue){
+
+        String sql = "INSERT INTO Student (ID,Name,LastName,Career,Tuition) VALUES ('"+stu.getID()+"','"+stu.getName()+"','"+stu.getLastName()+"','"+stu.getCareer()+"','"+stu.getTuition()+"')";
+
+        try (Connection conn = CMySQL.conectarMySQL()){
+
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+             int filasActualizadas = pstmt.executeUpdate();
+
+            if (filasActualizadas > 0) {
+                InsertTableBitacora(CMySQL,idValue,"Insertó un nuevo estudiante");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void DeleteInTableStudent(ConnectionMySQL CMySQL,Student stu,String idValue){
 
         String sql = "DELETE FROM Student WHERE ID = '"+stu.getID()+"'";
 
@@ -136,13 +211,48 @@ public class Data_Manipulator {
             int filasAfectadas = pstmt.executeUpdate();
             
             if (filasAfectadas > 0) {
-                System.out.println("Registro eliminado exitosamente.");
-            } else {
-                System.out.println("No se encontró ningún registro con ese ID.");
+                InsertTableBitacora(CMySQL,idValue,"Eliminó un estudiante");
             }
 
         } catch (SQLException e) {
             System.err.println("Error al eliminar datos: " + e.getMessage());
+        }
+    }
+
+    public void modifyTableStudent(ConnectionMySQL CMySQL,Student stu,String idValue){
+
+        String sql = "UPDATE Student SET Name='"+stu.getName()+"',LastName='"+stu.getLastName()+"',Career='"+stu.getCareer()+"',Tuition='"+stu.getTuition()+"' WHERE ID = '"+stu.getID()+"'";
+        try (Connection conn = CMySQL.conectarMySQL()){
+
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            int filasActualizadas = pstmt.executeUpdate();
+
+            if (filasActualizadas > 0) {
+                InsertTableBitacora(CMySQL,idValue,"Modificó un estudiante");
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error al modificar los datos: " + e.getMessage());
+        }
+    }
+    
+    
+    // Metodos de la tabla Reprobated
+    
+    // este metodo no guarda en la bitacora, ya que es automatio con el scanner de reprobados
+    public void InsertTableReprobated(ConnectionMySQL CMySQL,Reprobated rep){
+
+        String sql = "INSERT INTO Reprobated (IDStudent,CodeSubject,qualification,period) VALUES ('"+rep.getIDStudent()+"','"+rep.getCodeSubject()+"','"+rep.getGrade()+"','"+rep.getPeriod()+"')";
+
+        try (Connection conn = CMySQL.conectarMySQL()) {
+
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
     
@@ -166,69 +276,6 @@ public class Data_Manipulator {
             System.err.println("Error al eliminar datos: " + e.getMessage());
         }
     }
-    
-    // Métodos para modificar datos en la tablas
-
-    public void modifyTableUser(ConnectionMySQL CMySQL,User user){
-
-        String sql = "UPDATE User SET Name = '"+user.getName()+"',LastName = '"+user.getLastName()+"',Email = '"+user.getEmail()+"', Password = '"+user.getPassword()+"', Post = '"+user.getPost()+"' WHERE ID = '"+user.getID()+"'";
-
-        try (Connection conn = CMySQL.conectarMySQL()){
-
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-
-            int filasActualizadas = pstmt.executeUpdate();
-
-            if (filasActualizadas > 0) {
-                System.out.println("¡Registro modificado exitosamente!");
-            } else {
-                System.out.println("No se encontró un registro con ese ID.");
-            }
-
-        } catch (SQLException e) {
-            System.err.println("Error al modificar los datos: " + e.getMessage());
-        }
-    }
-
-    public void modifyTableSubject(ConnectionMySQL CMySQL,Subject sub){
-
-        String sql = "UPDATE Subject SET Code='"+sub.getCode()+"',Semester='"+sub.getSemester()+"',Unit_Credit='"+sub.getUnit_credit()+"',Name='"+sub.getName()+"'";
-        try (Connection conn = CMySQL.conectarMySQL()){
-
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-
-            int filasActualizadas = pstmt.executeUpdate();
-
-            if (filasActualizadas > 0) {
-                System.out.println("¡Registro modificado exitosamente!");
-            } else {
-                System.out.println("No se encontró un registro con ese ID.");
-            }
-
-        } catch (SQLException e) {
-            System.err.println("Error al modificar los datos: " + e.getMessage());
-        }
-    }
-
-    public void modifyTableStudent(ConnectionMySQL CMySQL,Student stu){
-
-        String sql = "UPDATE Student SET Name='"+stu.getName()+"',LastName='"+stu.getLastName()+"',Career='"+stu.getCareer()+"',Tuition='"+stu.getTuition()+"' WHERE ID = '"+stu.getID()+"'";
-        try (Connection conn = CMySQL.conectarMySQL()){
-
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-
-            int filasActualizadas = pstmt.executeUpdate();
-
-            if (filasActualizadas > 0) {
-                System.out.println("¡Registro modificado exitosamente!");
-            } else {
-                System.out.println("No se encontró un registro con ese ID.");
-            }
-
-        } catch (SQLException e) {
-            System.err.println("Error al modificar los datos: " + e.getMessage());
-        }
-    }
 
     public void modifyTableReprobated(ConnectionMySQL CMySQL,Reprobated rep){
 
@@ -249,5 +296,111 @@ public class Data_Manipulator {
             System.err.println("Error al modificar los datos: " + e.getMessage());
         }
     }
-}
     
+
+
+
+
+
+    
+
+
+
+
+    
+    //metodo para extraer datos de las tablas
+
+    public void ExtractTableUser(ConnectionMySQL CMySQL,User user){
+
+        String sql = "SELECT * FROM User WHERE ID = '"+user.getID()+"'";
+        try (Connection conn = CMySQL.conectarMySQL();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+
+            // Iterar sobre el ResultSet para extraer los datos
+            while (rs.next()) {
+                // Extrae datos por el nombre de la columna
+                String id = rs.getString("ID");
+                String name = rs.getString("Name");
+                String lastname = rs.getString("Lastname");
+                String email = rs.getString("email"); 
+
+                System.out.println(id + name);
+        }
+
+        } catch (SQLException e) {
+            System.err.println("Error al consultar los datos: " + e.getMessage());
+        }
+
+    }
+    
+    public void ExtractTableSubject(ConnectionMySQL CMySQL,Subject sub){
+
+    String sql = "SELECT * FROM Subject WHERE Code = '"+sub.getCode()+"'";  
+
+    try(Connection conn = CMySQL.conectarMySQL()){
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery();
+            while(rs.next()){
+                String code = rs.getString("Code");
+                String name = rs.getString("Name");
+                int semester = rs.getInt("Semester");
+                int unit_credit = rs.getInt("Unit_Credit");
+            }
+            }catch(SQLException e){
+    System.err.println("Error al consultar los datos: " + e.getMessage());
+   }
+    }
+
+    public void ExtractTableStudent(ConnectionMySQL CMySQL,Student stu){
+
+    String sql = "SELECT * FROM Student WHERE ID= '"+stu.getID()+"'";
+
+    try (Connection conn = CMySQL.conectarMySQL()){
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        ResultSet rs = pstmt.executeQuery();
+
+        while(rs.next()){
+            
+            String name = rs.getString("name");
+            String lastname = rs.getString("lastname");
+            String career = rs.getString("career");
+            String tuition = rs.getString("tuition");
+        }  
+    } catch (SQLException e) {
+        e.printStackTrace();
+        
+    }
+
+
+
+    }
+    
+    public void ExtractTableReprobated(ConnectionMySQL CMySQL,Reprobated rep){
+        String sql = "SELECT stu.ID, stu.Name, sub.Code, sub.name FROM reprobated rep "
+        +" INNER JOIN Student stu ON rep.IDStudent = stu.ID "
+        +" INNER JOIN Subject sub ON rep.CodeSubject = sub.Code "
+        +" WHERE rep.IDStudent = '32066670';";
+
+        try (Connection conn = CMySQL.conectarMySQL();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery()){
+        
+        while(rs.next()){
+        int ID = rs.getInt("ID");
+        int Code = rs.getInt("Code");
+        String nameSubject = rs.getString("Name");
+        String nameStudent = rs.getString("Name");
+
+        System.out.println(ID + Code + nameStudent + nameSubject +"1");
+        }    
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+        
+    }
+
+
+
+}
