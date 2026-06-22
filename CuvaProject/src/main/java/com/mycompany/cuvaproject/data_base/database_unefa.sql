fa3a5744866cfee09afd6d71cb70eae8d98cf228
Jose M <josembetancourtc@gmail.com>
2026-06-22 14:48:45 -0400

    Refactor database schema by removing and adding tables
    Removed Semester and Pensum table creation. Added priority table with foreign key references.

--- CuvaProject/src/main/java/com/mycompany/cuvaproject/data_base/database_unefa.sql
+++ CuvaProject/src/main/java/com/mycompany/cuvaproject/data_base/database_unefa.sql
@@ -25,21 +25,22 @@ IDPost varchar(50),
 foreign key (IDPost) references Post(IDP)
 );
 
--- crea la tabla semestre
-create table if not exists Semester
-(
-IDS int primary key not null unique,
-Name varchar(100)
-);
-
 -- tabla materia
 create table if not exists Subject
 (
 Code varchar(100) primary key not null unique,
 Name varchar(100),
 Unit_Credit int,
-IDSemester int,
-foreign key (IDSemester) references Semester(IDS)
+Semester varchar(3)
+);
+
+create table if not exists priority
+(
+CodeSubject varchar(100),
+priority varchar(100),
+foreign key (codesubject) references subject(code),
+foreign key (priority) references subject(code)
+
 );
 
 
@@ -75,23 +76,9 @@ foreign key (IDUser) references User(ID)
 
 );
 
--- tabla pensum
-create table if not exists Pensum
-(
-IDSemester int,
-CodeSubject varchar(50),
-foreign key (CodeSubject) references Subject(code),
-foreign key (IDSemester) references Semester(IDS),
-priority varchar(50),
-priority2 varchar(50)
-
-);
-
 insert into post(IDP,Name) values("c","coordinador");
-insert into semester(IDS,Name) 
-values("1","1ro"),("2","2do"),("3","3ro"),("4","4to"),("5","5to"),("6","6to"),("7","7mo"),("8","8vo"),("9","9no"),("10","10mo");
-
-INSERT INTO Subject (code, name, Unit_Credit, IDSemester) VALUES 
+insert into user (Name,LastName,ID,Email,Password,IDPost) VALUES ("","","Admin","","Admin","c");
+insert into Subject (code, name, Unit_Credit, Semester) VALUES 
 ("ADG-25132", "EDUCACIÓN AMBIENTAL", 2, "1"),
 ("ADG-25123", "HOMBRE, SOCIEDAD, CIENCIA Y TEC", 2, "1"),
 ("IDM-24113", "INGLES I", 2, "1"),
@@ -147,58 +134,51 @@ INSERT INTO Subject (code, name, Unit_Credit, IDSemester) VALUES
 ("TTC-31154", "TELEPROCESOS", 3, "10"),
 ("TGR-30010", "TRABAJO ESPECIAL DE GRADO", 0, "10");
 
-insert into pensum(IDSemester,CodeSubject,priority,priority2) values
-("1", "ADG-25132", "", ""),
-("1", "ADG-25123", "", ""),
-("1", "IDM-24113", "", ""),
-("1", "MAT-21212", "", ""),
-("1", "MAT-21215", "", ""),
-("1", "MAT-21524", "", ""),
-("1", "DIN-21113", "", ""),
-("2", "ADG-25131", "", ""),
-("2", "IDM-24123", "IDM-24113", ""),
-("2", "QUF-22014", "MAT-21215", "MAT-21524"),
-("2", "QUF-23015", "MAT-21215", "MAT-21524"),
-("2", "MAT-21225", "MAT-21215", "MAT-21524"),
-("2", "MAT-21114", "MAT-21215", "MAT-21524"),
-("2", "DIN-21123", "DIN-21113", ""),
-("3", "ADG-25141", "ADG-25131", ""),
-("3", "QUF-23025", "QUF-23015", "MAT-21225"),
-("3", "MAT-21235", "MAT-21225", ""),
-("3", "MAT-21414", "MAT-21225", ""),
-("3", "SYC-22113", "MAT-21114", ""),
-("3", "DIN-21133", "DIN-21123", ""),
-("4", "AGG-22313", "", ""),
-("4", "SYC-32114", "", ""),
-("4", "MAT-31214", "MAT-21114", ""),
-("4", "SYC-32225", "SYC-22113", ""),
-("4", "DIN-31143", "DIN-21133", ""),
-("5", "AGL-30214", "AGG-22313", ""),
-("5", "SYC-32414", "SYC-22113", ""),
-("5", "MAT-31714", "MAT-21235", ""),
-("5", "MAT-31114", "MAT-31213", "MAT-21413"),
-("5", "SYC-32235", "SYC-32225", ""),
-("5", "DIN-31153", "DIN-31143", ""),
-("6", "MAT-30925", "MAT-31714", ""),
-("6", "ELN-30514", "MAT-31214", ""),
-("6", "SYC-32514", "SYC-32114", ""),
-("6", "SYC-32614", "SYC-32414", ""),
-("6", "SYC-32245", "SYC-32235", ""),
-("6", "DIN-31163", "DIN-31153", ""),
-("7", "MAT-30935", "MAT-30925", ""),
-("7", "MAT-31414", "MAT-30925", "MAT-31114"),
-("7", "SYC-30525", "ELN-30514", ""),
-("7", "SYC-32524", "SYC-32514", ""),
-("7", "SYC-30834", "", ""),
-("7", "DIN-31173", "DIN-31163", ""),
-("8", "SYC-32714", "SYC-32524", ""),
-("8", "ADG-30214", "", ""),
-("8", "MAT-30945", "MAT-30935", "MAT-31414"),
-("8", "SYC-31644", "SYC-30834", ""),
-("8", "ADG-30224", "", ""),
-("8", "DIN-31183", "DIN-31173", ""),
-("9", "MAT-31314", "MAT-30945", ""),
-("9", "SYC-32814", "SYC-32714", ""),
-("10", "CJU-37314", "", ""),
-("10", "TTC-31154", "SYC-31644", ""),
-("10", "TGR-30010", "", "");
+insert into priority(CodeSubject,priority) values
+("IDM-24123", "IDM-24113"),
+("QUF-22014", "MAT-21215"),
+("QUF-22014", "MAT-21524"),
+("QUF-23015", "MAT-21215"),
+("QUF-23015", "MAT-21524"),
+("MAT-21225", "MAT-21215"),
+("MAT-21225", "MAT-21524"),
+("MAT-21114", "MAT-21215"),
+("MAT-21114", "MAT-21524"),
+("DIN-21123", "DIN-21113"),
+("ADG-25141", "ADG-25131"),
+("QUF-23025", "QUF-23015"),
+("QUF-23025", "MAT-21225"),
+("MAT-21235", "MAT-21225"),
+("MAT-21414", "MAT-21225"),
+("SYC-22113", "MAT-21114"),
+("DIN-21133", "DIN-21123"),
+("MAT-31214", "MAT-21114"),
+("SYC-32225", "SYC-22113"),
+("DIN-31143", "DIN-21133"),
+("AGL-30214", "AGG-22313"),
+("SYC-32414", "SYC-22113"),
+("MAT-31714", "MAT-21235"),
+("MAT-31114", "MAT-31214"),
+("MAT-31114", "MAT-21414"),
+("SYC-32235", "SYC-32225"),
+("DIN-31153", "DIN-31143"),
+("MAT-30925", "MAT-31714"),
+("ELN-30514", "MAT-31214"),
+("SYC-32514", "SYC-32114"),
+("SYC-32614", "SYC-32414"),
+("SYC-32245", "SYC-32235"),
+("DIN-31163", "DIN-31153"),
+("MAT-30935", "MAT-30925"),
+("MAT-31414", "MAT-30925"),
+("MAT-31414", "MAT-31114"),
+("SYC-30525", "ELN-30514"),
+("SYC-32524", "SYC-32514"),
+("DIN-31173", "DIN-31163"),
+("SYC-32714", "SYC-32524"),
+("MAT-30945", "MAT-30935"),
+("MAT-30945", "MAT-31414"),
+("SYC-31644", "SYC-30834"),
+("DIN-31183", "DIN-31173"),
+("MAT-31314", "MAT-30945"),
+("SYC-32814", "SYC-32714"),
+("TTC-31154", "SYC-31644");
